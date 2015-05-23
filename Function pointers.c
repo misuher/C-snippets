18f78abf66a522c66ed1d3ecc5e18651548b763d
misuher <cm.castillo87@gmail.com>
2015-05-23 14:11:47 +0200

    forEach functionality

--- Function pointers.c
+++ Function pointers.c
@@ -32,3 +32,41 @@ pfunc myfunction(int parameter1,int  parameter2){
     pfunc myfunctionpointer = &sum;
     return myfunctionpointer;
 }
+
+
+
+
+
+//example using function pointer to emulate "forEach"
+#include <stdio.h>
+#define MAX_COLORS  256
+
+typedef struct {
+    char* name;
+    int red;
+    int green;
+    int blue;
+} Color;
+
+Color Colors[MAX_COLORS];
+
+void eachColor (void (*fp)(Color *c)) {
+    int i;
+    for (i=0; i<MAX_COLORS; i++)
+        (*fp)(&Colors[i]);
+}
+
+void printColor(Color* c) {
+    if (c->name)
+        printf("%s = %i,%i,%i\n", c->name, c->red, c->green, c->blue);
+}
+
+int main() {
+    Colors[0].name="red";
+    Colors[0].red=255;
+    Colors[1].name="blue";
+    Colors[1].blue=255;
+    Colors[2].name="black";
+
+    eachColor(printColor);
+}
